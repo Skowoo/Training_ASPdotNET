d704ac48aa11fac45982b54a9997a61eb045a0d9
Skowoo <skowoo@tlen.pl>
2024-02-07 10:08:16 +0100

    Lab1 done

--- AplikacjaLaby/Controllers/HomeController.cs
+++ AplikacjaLaby/Controllers/HomeController.cs
@@ -8,6 +8,50 @@ namespace AplikacjaLaby.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public enum Operator
+        {
+            Unknown, Add, Mul, Sub, Div
+        } //Unknown wy³apuje wszystkie wartoœci spoza podanych czterech!
+
+        public IActionResult Calculator(Operator op, string a, string b) //stringi pobierane s¹ z Query (pasek adresu)
+        {
+            bool aResult = double.TryParse(a, out double aParsed);
+            ViewBag.a = aParsed;
+            bool bResult = double.TryParse(b, out double bParsed);
+            ViewBag.b = bParsed;
+            ViewBag.Op = op;
+            double? result = null;
+
+            if (aResult && bResult)
+            {
+                switch (op) 
+                {
+                    case Operator.Add:
+                        result = aParsed + bParsed;
+                        break;
+                    case Operator.Mul:
+                        result = aParsed * bParsed;
+                        break;
+                    case Operator.Sub:
+                        result = aParsed - bParsed;
+                        break;
+                    case Operator.Div:
+                        result = aParsed / bParsed;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            result ??= double.NaN;
+
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+        public IActionResult About() => View();
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
