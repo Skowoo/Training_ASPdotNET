df8ec2387d9dc9a42efc32db429e8beeed7fe926
Skowoo <skowoo@tlen.pl>
2024-03-26 12:09:44 +0100

    cleanup

--- Egzamin_Rectangle1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Egzamin_Rectangle1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
